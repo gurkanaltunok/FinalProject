021f8c873f2703492f4dbc062e8a972087b58200
Oğuz Gürkan Altunok <oguzgurkan2001@gmail.com>
2021-01-31 00:17:04 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
