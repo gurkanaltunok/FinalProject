cf79552198644d2631f3d73dcc840986fdcea103
Oğuz Gürkan Altunok <oguzgurkan2001@gmail.com>
2021-02-06 23:57:49 +0300

    06.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,14 +9,38 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //ProductTest();
+            //CategoryTest();
+
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductId + "/" + product.ProductName + "/" + product.CategoryName);
+            }
+
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
             foreach (var product in productManager.GetAllByCategoryId(2))
             {
                 Console.WriteLine(product.ProductName);
             }
-            foreach (var product in productManager.GetByUnitPrice(15,35))
+            foreach (var product in productManager.GetByUnitPrice(15, 35))
             {
-                Console.WriteLine("Ürün Adı: {0}, Ürün Fiyatı: {1} ",product.ProductName, product.UnitPrice);
+                Console.WriteLine("Ürün Adı: {0}, Ürün Fiyatı: {1} ", product.ProductName, product.UnitPrice);
             }
         }
     }
